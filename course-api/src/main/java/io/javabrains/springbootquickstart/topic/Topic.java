328960b79036c2c2710fdf369c4b70284101e9da
pdebray <135981373+pdebray@users.noreply.github.com>
2023-08-28 18:31:36 +0200

    Add files via upload

---
+++ course-api/src/main/java/io/javabrains/springbootquickstart/topic/Topic.java
@@ -0,0 +1,16 @@
+package io.javabrains.springbootquickstart.topic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Topic {
+	
+	private String id;
+	private String name;
+	private String description;
+	
+
+	
+}
